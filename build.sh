304d8faf37f9fa5d49bfb42895561cd721c16b23
Gerd v. Egidy <gerd@egidy.de>
2022-11-05 17:53:24 +0100

    add some safety checks

--- build.sh
+++ build.sh
@@ -32,7 +32,7 @@ cp -R ${HERE}/AppDirSrc/* ${HERE}/AppDirBuild/
 # install binaries
 declare -a install_bins
 install_bins=(
-    xorriso sfdisk syslinux mcopy mformat mtools bash dd mktemp
+    xorriso sfdisk syslinux mcopy mformat mtools bash dd mktemp lsblk
 )
 
 for bin in "${install_bins[@]}"; do
@@ -60,6 +60,9 @@ install_libs=(
     libsmartcols
     libuuid
     libblkid
+    libmount
+    libudev
+    libgcc_s
 )
 
 for lib in "${install_libs[@]}"; do
